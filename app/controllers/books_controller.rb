fa8255d32d3eb490f295576f1f88da6a8c61170a
Megumu-Suzuki <m.suzuki.te@gmail.com>
2021-07-17 14:12:56 +0000

    [Update]4-10

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,18 +1,45 @@
 class BooksController < ApplicationController
 
-  def new
+  def index
+    @book = Book.new
+    @books = Book.all
+
   end
 
   def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to book_path(@book.id)
   end
 
-  def index
-  end
+
 
   def show
+    @book = Book.new
+    @book = Book.find(params[:id])
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book.params)
+    redirect_to book_path(book.id)
   end
 
   def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
   end
-  
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body, :user_id)
+  end
+
 end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '1abbab8b3dade6ccb7b312cd5d82b10bb30be1d9708c4dc402093523ab08a1cb42576dac0019ba2b641f9f0b393aa84ab93c27be3769c2cfbb3ae0d701c65d05'
